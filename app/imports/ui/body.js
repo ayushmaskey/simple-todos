925f440f3a99ce7c897b8cb41e83b35372e1c87f
amaskey <maskey.maskey@gmail.com>
2017-10-09 21:26:03 -1000

    form

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -6,6 +6,28 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, {sort: {createdAt: -1 } });
   },
+});
+
+Template.body.events({
+
+  //key value pair of event {submit: value Entered in form}
+  'submit .new-task'(event) {
+    //prevents default browser form submit??
+    event.preventDefault();
+
+    //get value from form - event
+    const text = event.target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    // console.log(event); // see the values available in event
+
+    //clear form
+    event.target.text.value = '';
+  }
 })
