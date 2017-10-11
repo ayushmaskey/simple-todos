af97e3340ebcb1cfb15fa6a1593e672b5fa82ff9
amaskey <maskey.maskey@gmail.com>
2017-10-10 18:39:07 -1000

    temp state UI

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,15 +1,29 @@
 
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({         //used to pass data into template
 
   tasks() {
+    const instance = Template.instance();
+
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find( { checked: { $ne: true}} , {sort: { createdAt: -1 }});
+    }
     return Tasks.find({}, {sort: {createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find( { checked: { $ne: true} }).count();
   },
 
 });
@@ -30,6 +44,10 @@ Template.body.events({      //watch of any of the events in whole body tag
     // console.log(event);            // see the values available in event
 
     event.target.text.value = '';       //clear form
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 
 
